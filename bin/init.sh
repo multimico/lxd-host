f28a3e8da79dd21a68af5de52fc529beb6a12058
Christian Glahn <cpglahn@gmail.com>
2022-05-09 10:02:56 +0200

    reorder tasks

--- bin/init.sh
+++ bin/init.sh
@@ -7,16 +7,28 @@ MACADDRESS=$1
 PACKAGES=$(yq ".packages[]" ${DATADIR}/init.yaml)
 apt install -y $PACKAGES
 
-cp ${DATADIR}/preseed.yaml /etc/lxd/preseed.yaml
+### 
+# OVS and Network setup 
 
+# add bridging switch ovs0 
 ovs-vsctl add-br ovs0
+
+# create  host interface mgnt0 and add it to switch ovs0
 ovs-vsctl add-port ovs0 mgnt0 -- set interface mgnt0 type=internal
+
+# add phys hardware (eno1) to switch ovs0
+ovs-vsctl add-port ovs0 eno1
+
+# At this point the mgnt0 device should be able to receive an IP address
+
 # fix netplan
 sed -i -e "s/dhcp4: true/dhcp4: no/"  -e "/^  version: 2/i \\ \\ \\ \\ mgnt0:\\n\\ \\ \\ \\ \\ \\ dhcp4: true\\n\\ \\ \\ \\ \\ \\ macaddress: $MACADDRESS" /etc/netplan/00-installer-config.yaml
+# activate new netplan configuration
 netplan apply
-# activate phys hardware
-ovs-vsctl add-port ovs0 eno1
+
+### 
 # Init lxd
+cp ${DATADIR}/preseed.yaml /etc/lxd/preseed.yaml
 cat /etc/lxd/preseed.yaml | lxd init --preseed
 
 echo "Succeeded"
